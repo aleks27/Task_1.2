afbbc1b2ffff22c4920a158da843f5aad86ae2e0
Aleks Nechayev <aleks.nechayev@gmail.com>
2016-12-13 22:19:55 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >=0; i--) {
+            System.out.print(args[i] + " ");
+        }
+    }
+}
